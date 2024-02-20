066ebd3cae6a3f3a37f2d82a48244d620a2b8b7f
Omer <onoor@uwo.ca>
2024-02-19 23:10:00 -0500

    Removing broken files

--- app/user/landing/page.tsx
+++ app/user/landing/page.tsx
@@ -24,7 +24,7 @@ export default async function UserLanding() {
             <h1 className="text-4xl font-bold">User Information</h1>
             <div className="flex flex-col border rounded-lg gap-2 mt-5 p-6">
                 <div className="flex flex-row gap-5">
-                    <Avatar src={dbUser?.avatarPath} size="lg" />
+                    <Avatar src={dbUser?.avatarPath ?? ""} size="lg" />
                     <div className="flex flex-col gap-0">
                         <p className="text-3xl font-medium font-bold">{dbUser?.username}</p>
                         <p className="text-xs">Regular User</p>
@@ -38,7 +38,7 @@ export default async function UserLanding() {
                     <p className="font-medium">name: </p>
                     <p>{dbUser?.name}</p>
                 </div>
-                <UserUploadComponent userId={dbUser?.id} />
+                <UserUploadComponent userId={dbUser?.id ?? 0} />
             </div>
         </div>
     )
